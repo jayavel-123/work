100efb7d6a35b8b071884a71ab9e6c6933387098
jayavel-123 <jayavel22td0683@svcet.ac.in>
2025-01-29 16:31:58 +0530

    Add files via upload

---
+++ work2.py
@@ -0,0 +1,7 @@
+idly=int(input("How many idlies ate today:"))
+if idly>=3 and idly<=2:
+    print("hungry")
+elif idly>=3 and idly<=5:
+    print("sleep")
+else:
+    print("angry")
---
+++ work3.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the input a:"))
+b=int(input("Enter the input b:"))
+print(a+b)
